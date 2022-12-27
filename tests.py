7c2cd2860f87740c338dc62c597a2bdf8155d8e3
wmadison <wmadison@student.21-school.ru>
2022-12-27 19:51:53 +0500

    add Days Between

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 """ TESTS for all """
 from FirstWord import first_word
 from RightToLeft import left_join
+from DaysBetween import days_diff
 
 
 def test_right_to_left():
@@ -43,3 +44,17 @@ def test_first_word():
     assert first_word(". ") == ""
     assert first_word(" ") == ""
     assert first_word("") == ""
+
+
+def test_days_days_diff():
+    assert days_diff((1982, 4, 19), (1982, 4, 22)) == 3
+    assert days_diff((2014, 1, 1), (2014, 8, 27)) == 238
+    assert days_diff((2014, 8, 27), (2014, 1, 1)) == 238
+    assert days_diff((14, 1, 27), (14, 1, 1)) == 26
+    assert days_diff((1992, 10, 28), (1989, 6, 23)) == 1223
+    assert days_diff((1622, 10, 4), (3555, 10, 12)) == 706021
+    assert days_diff((5871, 8, 23), (6155, 6, 14)) == 103659
+    assert days_diff((4632, 11, 18), (8077, 10, 26)) == 1258238
+    assert days_diff((696, 5, 7), (9241, 6, 27)) == 3121048
+    assert days_diff((6051, 1, 23), (4129, 8, 9))
+
