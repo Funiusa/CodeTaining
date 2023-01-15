80a5d81e2fabaf6574763fc47abf884ab11add92
wmadison <wmadison@student.21-school.ru>
2023-01-15 16:52:47 +0500

    add Absolute sorted

---
+++ safe_pawns.py
@@ -0,0 +1,10 @@
+def safe_pawns(crd: set) -> int:
+    s = sorted(list(crd))
+    if s[0][0] == 'a':
+        if s[1][0] == 'a'
+
+    return 0 # TODO
+
+
+if __name__ == "__main__":
+    safe_pawns({"a4", "a3", "b3", "f4"})
