8283130f81553dd08b93e4839eaf9196522e96a9
wmadison <wmadison@student.21-school.ru>
2023-01-15 23:59:30 +0500

    add time_converter

---
+++ SumByType.py
@@ -0,0 +1,16 @@
+"""
+    You have a list.
+    Each value from that list can be either a string or an integer.
+    Your task here is to return two values.
+    The first one is a concatenation of all strings from the given list.
+    The second one is a sum of all integers from the given list.
+
+    Input: A list of strings and integers.
+
+    Output: A list or tuple.
+"""
+
+def sum_by_type(items: list[str, int]) -> tuple[str, int] | list[str, int]:
+   return []
+
+
--- TimeConverter.py
+++ TimeConverter.py
@@ -1,3 +1,17 @@
+"""
+    You prefer a good old 12-hour time format.
+    But the modern world we live in would rather use the 24-hour format and you see it everywhere.
+    Your task is to convert the time from the 24-h format into 12-h format by following the next rules:
+
+    - the output format should be 'hh:mm a.m.' (for hours before midday) or 'hh:mm p.m.' (for hours after midday);
+    - if hours is less than 10 - don't write a '0' before it. For example: '9:05 a.m.'.
+
+    Input: Time in the 24-hour format (as a string).
+
+    Output: Time in the 12-hour format (as a string).
+"""
+
+
 def time_converter(time: str) -> str:
     t = time.split(':')
     hour, minute = int(t[0]), t[1]
@@ -5,14 +19,3 @@ def time_converter(time: str) -> str:
         return f"{hour - 12 if hour != 12 else '12'}:{minute} p.m."
     return f"{hour if hour > 0 else '12'}:{minute} a.m."
 
-
-if __name__ == "__main__":
-    print(time_converter("12:30"))
-    print(time_converter("09:00"))
-    print(time_converter("02:45"))
-    print(time_converter("22:54"))
-    print(time_converter("17:12"))
-    print(time_converter("00:01"))
-    print(time_converter("13:10"))
-    print(time_converter("23:59"))
-    print(time_converter("00:00"))
